d3ec09ef092e794df9b99b067401c90750e5bda8
kota-kumar <kotasaikumar.ksk444@gmail.com>
2022-10-17 20:39:53 +0530

    update

--- src/Ifelse.java
+++ src/Ifelse.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 public class Ifelse {
     public static void main(String[] args) {
-        // Write a program to input two numbers(A & B) from user and print the maximum element among A & B in each line.
+ //1.  Write a program to input two numbers(A & B) from user and print the maximum element among A & B in each line.
     Scanner Temp = new Scanner(System.in);
     int A = Temp.nextInt();
     int B = Temp.nextInt();
@@ -16,7 +16,7 @@ public class Ifelse {
         System.out.print(B+ " is the Maximum number");
           }
 
-//Write a program to input two numbers(A & B) from user and print the minimum element among A & B in each line.
+//2.  Write a program to input two numbers(A & B) from user and print the minimum element among A & B in each line.
 
     Scanner temp = new Scanner(System.in);
 
@@ -30,7 +30,7 @@ public class Ifelse {
          System.out.print(B+ " is the Minimum number");
      }
 
-  //Write a program to input an integer from user and print whether given number is Even or Odd
+ //3.   Write a program to input an integer from user and print whether given number is Even or Odd
 
       Scanner temp = new Scanner(System.in);
        int N = temp.nextInt();
@@ -43,7 +43,7 @@ public class Ifelse {
            System.out.print(N+ " is Odd number");
        }
 
- //You are given the Cost Price C and Selling Price S of a Product. You have to tell whether there is a Profit or Loss. Also, calculate total profit or loss.
+ //4.    You are given the Cost Price C and Selling Price S of a Product. You have to tell whether there is a Profit or Loss. Also, calculate total profit or loss.
 
        Scanner temp = new Scanner(System.in);
         int C = temp.nextInt();
@@ -60,7 +60,7 @@ public class Ifelse {
             System.out.print("you got a loss of " +loss);
         }
 
-//You are given an integer A as input. You have to tell whether A is divible by both 5 and 11 or not.
+//5.      You are given an integer A as input. You have to tell whether A is divible by both 5 and 11 or not.
 
       Scanner temp = new Scanner(System.in);
         int A = temp.nextInt();
@@ -73,7 +73,7 @@ public class Ifelse {
             System.out.print(A+ " is not divible by both 5 and 11");
         }
 
-//Write a program that asks the user to input their age and tells them if they're old enough to ride a roller coaster. The minimum age to ride the roller coaster in this question is 13.
+//6.      Write a program that asks the user to input their age and tells them if they're old enough to ride a roller coaster. The minimum age to ride the roller coaster in this question is 13.
 
        Scanner temp = new Scanner(System.in);
         int A = temp.nextInt();
@@ -86,7 +86,7 @@ public class Ifelse {
             System.out.print("You can't ride the roller coaster.");
         }
 
-//You have been given a dataset for marks of 2 subjects, scored by students of classes ClassA and ClassB. You now want to compare the performances of class A and class B by finding out the average performance of the classes. Write a program to find if class A performed better. Print True is Class A is strictly better else return False.
+//7.      You have been given a dataset for marks of 2 subjects, scored by students of classes ClassA and ClassB. You now want to compare the performances of class A and class B by finding out the average performance of the classes. Write a program to find if class A performed better. Print True is Class A is strictly better else return False.
 
         Scanner temp = new Scanner(System.in);
         double A1 = temp.nextDouble();
@@ -104,7 +104,7 @@ public class Ifelse {
             System.out.print("class B is strictly better than A");
         }
 
- /*Given a Number N, print the answer according to the following rules-
+ /*8.   Given a Number N, print the answer according to the following rules-
 1. If N is divisible by 3 print Rock
 2. If N is divisible by 5 print star
 3. If N is divisible by both 3 and 5 print Rockstar*/
@@ -122,7 +122,7 @@ public class Ifelse {
             System.out.print("Rock Star");
         }
 
- /*Mr. ST is in troubles in with numbers, he is confuse that number is positive-odd, positive-even, negative-odd OR negative-even.
+ /*9.   Mr. ST is in troubles in with numbers, he is confuse that number is positive-odd, positive-even, negative-odd OR negative-even.
 Write a code for him to figure out the Situation.
 Take a number N from user and print the Situation Of Number.
 if positive-odd : print -> "Number is Positive and Odd"
@@ -147,7 +147,7 @@ if negative-even : print -> "Number is Negative and Even"*/
             System.out.print("Number is Negative and Odd");
         }
 
-//Mr. T is struggling with a graph, he has a coordinate point (X,Y) but he doesn't which quadrant this point belongs to.
+//10.   Mr. T is struggling with a graph, he has a coordinate point (X,Y) but he doesn't which quadrant this point belongs to.
 //Write a program to help him figure out the Quadrant of the given X,Y coordinate.
 
        Scanner temp = new Scanner(System.in);
@@ -169,7 +169,7 @@ if negative-even : print -> "Number is Negative and Even"*/
             System.out.print(X+ " , " +Y+" belongs to Fourth Quadrant");
         }
 
-//A company decided to give bonus of 5% to employee if his/her year of service is more than 5 years. Ask user for their name and year of service and print whether they should recieve bonus or not.
+//11.   A company decided to give bonus of 5% to employee if his/her year of service is more than 5 years. Ask user for their name and year of service and print whether they should recieve bonus or not.
 
       Scanner temp = new Scanner(System.in);
 
@@ -184,7 +184,7 @@ if negative-even : print -> "Number is Negative and Even"*/
             System.out.print("No "+S+ " will not recieve bonus.");
         }
 
- //Exams are near and Rahul is worried whether he will be allowed to take the exams or not. A student is not allowed to take the exams if his/her attendance is less than 75%. But Rahul is a little weak in math, so he wants your help to tell him whether he will be able to give exams or not. You are given the total number of classes held (T) and number of classes attended by Rahul (N),
+ //12.  Exams are near and Rahul is worried whether he will be allowed to take the exams or not. A student is not allowed to take the exams if his/her attendance is less than 75%. But Rahul is a little weak in math, so he wants your help to tell him whether he will be able to give exams or not. You are given the total number of classes held (T) and number of classes attended by Rahul (N),
 
  Scanner temp = new Scanner(System.in);
         double Total_Classes = temp.nextDouble();
